fc703e910fb81d5c9229c8656e9bf3ced75240d7
0261 <bada013@naver.com>
2019-09-20 12:55:48 +0900

    feature: 카운터 리덕스 추가 (Duck pattern 리덕스)
    덕 패턴이 좋다. 여러 파일 건들면 귀찮다.

---
+++ src/store/counter.ts
@@ -0,0 +1,80 @@
+console.log('카운터 리듀서 생성');
+/**
+ * Action Type
+ * 액션 타입은 이름이 겹치지 않도록 앞에 폴더명을 작성해준다.
+ */
+const INCREMENT_COUNT = 'counter/INCREMENT_COUNT';
+const CHANGE_COLOR = 'counter/CHANGE_COLOR';
+
+interface IncrementAction {
+    type: typeof INCREMENT_COUNT;
+}
+interface ChangeAction {
+    type: typeof CHANGE_COLOR;
+    payload: {
+        color: string;
+    };
+}
+type CounterActionTypes = IncrementAction | ChangeAction;
+
+/**
+ * Action Creator
+ * 액션 생성 함수는 타입을 반환한다. 필요하다면 인수도 같이 반환한다.
+ */
+export const incrementCount = () => {
+    console.log('넘버 증가 액션 생성함수');
+    return { type: INCREMENT_COUNT };
+};
+export const changeColor = (color: string) => {
+    return {
+        type: CHANGE_COLOR,
+        payload: {
+            color,
+        },
+    };
+};
+
+const initState = {
+    payload: { color: 'red', number: 0 },
+};
+/**
+ * Reducer
+ * 리듀서는 상태를 직접 변경해주는 녀석이다.
+ * 이전 상태와 타입을 받아서 새로운 상태를 반환한다.
+ */
+export const counterReducer = (
+    state = initState,
+    action: CounterActionTypes,
+) => {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            console.log('넘버 증가 리듀서');
+            return {
+                ...state,
+                payload: {
+                    color: state.payload.color,
+                    number: state.payload.number + 1,
+                },
+            };
+        case CHANGE_COLOR:
+            return {
+                ...state,
+                payload: {
+                    color: action.payload.color,
+                    number: state.payload.number,
+                },
+            };
+
+        default:
+            return state;
+    }
+};
+
+//////////////////////////////////////////////////////////////////////////////
+///////
+//
+// view => action => middleware => reducer => store => view
+// 액션 생성자를 호출하고 액션생성자의 반환값이 리듀서를 호출하고 리듀서는 상태를 변경한 뒤 스토어에 저장한다.
+//
+///////
+//////////////////////////////////////////////////////////////////////////////
