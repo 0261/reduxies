e08dad80060b83d4ef9d1d65721ff89eda0c6758
0261 <bada013@naver.com>
2019-09-21 01:13:45 +0900

    feature: 라우터 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import CounterContainer from '../containers/Counter/Counter';
+import TodoContainer from '../containers/Todo/Todo';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 const App = () => {
     return (
         <div>
-            <CounterContainer></CounterContainer>
+            <BrowserRouter>
+                <ul>
+                    <li>
+                        <Link to='/'>counter</Link>
+                    </li>
+                    <li>
+                        <Link to='/todo'>todo</Link>
+                    </li>
+                </ul>
+                <Route exact path='/' component={CounterContainer}></Route>
+                <Route path='/todo' component={TodoContainer}></Route>
+            </BrowserRouter>
         </div>
     );
 };
