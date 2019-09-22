2fc6a28fa74833252420d191ccc2ec4e3127893b
0261 <bada013@naver.com>
2019-09-22 13:51:14 +0900

    build: 바벨6버전은 .babelrc 7버전은 babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/typescript',
+];
+const plugins = [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    [
+        '@babel/plugin-transform-runtime',
+        {
+            regenerator: true,
+        },
+    ],
+];
+
+module.exports = { presets, plugins };
